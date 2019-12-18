b021aa1c4323bb9d3c76a1629366f34b4b0d5aa0
Cheukting <cheukting.ho@gmail.com>
2019-12-18 14:40:46 +0000

    [Fix]Adding path in __init__ for packaging.

--- src/woqlclient/__init__.py
+++ src/woqlclient/__init__.py
@@ -1,7 +1,11 @@
 """
     woqlClient Library
     ~~~~~~~~~~~~~~~~~~~~~
+
 """
+import sys
+sys.path.append('src/woqlclient')
+sys.path.append('src/tests')
 name = "woqlclient"
 __version__ = "0.0.1"
 # cmdclass = {'test': PyTest}
