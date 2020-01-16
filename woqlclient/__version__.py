1d22a750088408104600452dd2088ee19d899e34
Cheukting <cheukting.ho@gmail.com>
2020-01-16 15:26:55 +0000

    Update License to meet pypi requirement

--- woqlclient/__version__.py
+++ woqlclient/__version__.py
@@ -6,5 +6,5 @@ __version__ = '0.0.3'
 __build__ = 00
 __author__ = 'TerminusDB group'
 __author_email__ = 'terminusdatabase@gmail.com'
-__license__ = 'Apache 2.0'
+__license__ = 'GNU General Public License v3 (GPLv3)'
 __copyright__ = 'Copyright 2019 datachemist'
