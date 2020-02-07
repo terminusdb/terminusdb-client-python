39bfe3f2616b9a2fe473a2a6e017b2459d1ad7d2
Cheukting <cheukting.ho@gmail.com>
2020-02-07 10:59:50 +0000

    Fix version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setuptools.setup(
     name="terminus-client-python",
-    version="0.0.8",
+    version="0.0.12",
     author="TerminusDB group",
     author_email="terminusdatabase@gmail.com",
     description="Python client for Terminus DB",
