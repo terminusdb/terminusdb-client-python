b6b1c5982200586e248de9de3073ebddd71b67c2
Cheukting <cheukting.ho@gmail.com>
2020-02-29 09:51:19 +0000

    make woqlDataframe requirement optional and display warning about it instead of error

--- woqlclient/__init__.py
+++ woqlclient/__init__.py
@@ -5,8 +5,17 @@
 """
 from .woqlClient import *
 from .woql import WOQLQuery
-from .woqlDataframe import *
 from .__version__ import __version__
+
+try:
+    from .woqlDataframe import *
+except ImportError as e:
+    msg = (
+        "woqlDataframe requirements are not installed.\n\n"
+        "If you want to use woqlDataframe, please pip install as follows:\n\n"
+        "  python -m pip install -U terminus-client-python[dataframe]"
+    )
+    raise ImportError(msg) from e
 
 __all__ = ['woqlClient', 'woql', 'woqlDataframe']
 
