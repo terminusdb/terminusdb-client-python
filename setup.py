7fd422ff52b31c1c6efa9a3acc7bea6ade4b6c62
Cheukting <cheukting.ho@gmail.com>
2020-02-29 11:04:55 +0000

    Bump version: 0.0.17 → 0.0.18

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ extras_require = {
 
 setuptools.setup(
     name="terminus-client-python",
-    version="0.0.17",
+    version="0.0.18",
     author="TerminusDB group",
     author_email="terminusdatabase@gmail.com",
     description="Python client for Terminus DB",
