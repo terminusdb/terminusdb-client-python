08fbb37168b04e1d6c39788c5f8d1e33bce7f723
Cheukting <cheukting.ho@gmail.com>
2020-08-06 03:56:34 +0100

    start writing WOQLObj and TerminusDB

--- terminusdb_client/woqlquery/smart_query.py
+++ terminusdb_client/woqlquery/smart_query.py
@@ -1,5 +1,6 @@
-from typing import Union
+from typing import Union, List
 from .woql_query import WOQLQuery
+from ..woqlclient import WOQLClient
 
 class WOQLClass:
     def __init__(self, id:str, label:str =None, description:str =None, property:dict = None):
@@ -57,8 +58,41 @@ class WOQLClass:
 
 
 class WOQLObj:
-    def __init__(self, id:str, type:WOQLClass, label:str =None, description:str =None):
+    def __init__(self, id:str, obj_type:WOQLClass, label:str =None, description:str =None, property:dict = None):
         self.id = id
+        self.woql_id = self._idgen(id)
         self.label = label
         self.description = description
-        self.type = type
+        self._type = obj_type
+
+        if property is not None:
+            self._property = property
+        else:
+            self._property = {}
+
+    def _idgen(self, id:str) -> str:
+        # mimic what a idgen would do in the back end
+        pass
+
+    def add_property(self, pro_id:str, pro_value: Union[str,'WOQLObj']):
+        # check if the pro_value matches the property of the self._type
+        # add new prop in self._property
+        pass
+
+class TerminusDB:
+    def __init__(
+        self, server_url:str, db_id:str, key:str ="root", account:str ="admin", user:str ="admin", db_label:str =None, db_description:str =None, **kwargs
+        ):
+
+        self._client = WOQLCLient(server_url , **kwargs)
+        self._client.connect(key, account, user)
+        existing = self._client.get_database(db_id, self._client.uid())
+        if not existing:
+            self._client.create_database(db_id, account, db_label, db_description)
+            self.classes = {}
+        else:
+            self._client.db(db_id)
+            #get all classes from db and store them
+
+    def add_class(self, obj:Union[WOQLClass,List[WOQLClass]]):
+        pass
