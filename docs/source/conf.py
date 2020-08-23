f90b05a654c99ad0e947e751a40de4283302a520
Cheukting <cheukting.ho@gmail.com>
2020-08-24 00:48:52 +0100

    adding type hint in doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ master_doc = "index"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme", "sphinxcontrib.napoleon"]
+extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme", "sphinxcontrib.napoleon", "sphinx_autodoc_typehints"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
