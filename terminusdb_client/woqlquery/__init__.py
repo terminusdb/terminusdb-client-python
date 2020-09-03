340241881281cebe1d3e10dc18571870446861a5
Cheukting <cheukting.ho@gmail.com>
2020-09-03 19:57:11 +0100

    fix import problem

--- terminusdb_client/woqlquery/__init__.py
+++ terminusdb_client/woqlquery/__init__.py
@@ -1,3 +1,3 @@
 from .smart_query import TerminusDB, WOQLClass, WOQLObj  # noqa
 from .woql_library import WOQLLib  # noqa
-from .woql_query import WOQLQuery  # noqa
+from .woql_query import WOQLQuery, TerminusDB, WOQLClass, WOQLObj  # noqa
