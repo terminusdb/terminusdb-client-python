dfc0a11cffd7d66d8ce96c3d550f6d5422731396
Cheukting <cheukting.ho@gmail.com>
2020-09-03 16:11:39 +0100

    really switch off integration tests

--- terminusdb_client/tests/integration_tests/test_smart_query.py
+++ terminusdb_client/tests/integration_tests/test_smart_query.py
@@ -2,10 +2,6 @@ import os
 import subprocess
 
 import pytest
-import requests
-
-from terminusdb_client.woqlquery.smart_query import TerminusDB, WOQLClass, WOQLObj
-from terminusdb_client.woqlquery.woql_library import WOQLLib
 
 from .ans_test_db import *  # noqa
 
@@ -42,34 +38,35 @@ def docker_url(pytestconfig):
     subprocess.run(["docker-compose", "rm", "--force", "--stop", "-v"])
 
 
-def test_main_service_run(docker_url):
-    result = requests.get(docker_url)
-    assert result.status_code == 200
-
-
-def test_init_terminusdb(docker_url):
-    db = TerminusDB(docker_url, "test")
-    assert db._client.db("test") == "test"
-
-
-def test_add_class_and_object(
-    docker_url, one_class_obj, one_class_prop, one_object, one_prop_val
-):
-    db = TerminusDB(docker_url, "test")
-    my_class = WOQLClass(
-        "Journey",
-        label="Bike Journey",
-        description="Bike Journey object that capture each bike joourney.",
-    )
-    my_class.add_property("Duration", "integer")
-    db.add_class(my_class)
-
-    assert db.run(WOQLLib().classes()) == one_class_obj
-    assert db.run(WOQLLib().property()) == one_class_prop
-
-    my_obj = WOQLObj("myobj", my_class)
-    my_obj.add_property("Duration", 75)
-    db.add_object(my_obj)
-
-    assert db.run(WOQLLib().objects()) == one_object
-    assert db.run(WOQLLib().property_values()) == one_prop_val
+#
+# def test_main_service_run(docker_url):
+#     result = requests.get(docker_url)
+#     assert result.status_code == 200
+#
+#
+# def test_init_terminusdb(docker_url):
+#     db = TerminusDB(docker_url, "test")
+#     assert db._client.db("test") == "test"
+#
+#
+# def test_add_class_and_object(
+#     docker_url, one_class_obj, one_class_prop, one_object, one_prop_val
+# ):
+#     db = TerminusDB(docker_url, "test")
+#     my_class = WOQLClass(
+#         "Journey",
+#         label="Bike Journey",
+#         description="Bike Journey object that capture each bike joourney.",
+#     )
+#     my_class.add_property("Duration", "integer")
+#     db.add_class(my_class)
+#
+#     assert db.run(WOQLLib().classes()) == one_class_obj
+#     assert db.run(WOQLLib().property()) == one_class_prop
+#
+#     my_obj = WOQLObj("myobj", my_class)
+#     my_obj.add_property("Duration", 75)
+#     db.add_object(my_obj)
+#
+#     assert db.run(WOQLLib().objects()) == one_object
+#     assert db.run(WOQLLib().property_values()) == one_prop_val
