a5a965f731ed47d6edbe6d11abe75f3c6a4f071e
Bojan Miletic <4889922+Morijarti@users.noreply.github.com>
2020-09-06 10:16:51 +0200

    Resolved issue with docker fixture not starting up on time during travis execution

--- terminusdb_client/tests/integration_tests/test_smart_query.py
+++ terminusdb_client/tests/integration_tests/test_smart_query.py
@@ -1,10 +1,13 @@
 import os
 import subprocess
+import time
 
 import pytest
 import requests
 
 from terminusdb_client.woqlquery.smart_query import TerminusDB
+
+MAX_CONTAINER_STARTUP_TIME = 30
 
 
 @pytest.fixture(scope="module")
@@ -20,23 +23,49 @@ def docker_url(pytestconfig):
             "up",
             "-d",
         ],
-        stderr=subprocess.PIPE,
+        stderr=subprocess.PIPE
     )
     if output.returncode != 0:
-
         raise RuntimeError(output.stderr)
-    # is_server_started = False
-    #
-    # # while not is_server_started:
-    # #     service = subprocess.run(["docker-compose", "--file",  f"{docker_compose_path}", "ps", "--services", "--filter",
-    # #                               "status=running", "|", "grep", "terminusdb-server"]
-    # #                              , stdout=subprocess.PIPE, check=True)
-    # #     print(service.stdout)
-    # #     is_server_started = True
+
     test_url = "http://127.0.0.1:6363"
+    is_server_started = False
+
+    seconds_waited = 0
+    while not is_server_started:
+        service = subprocess.run(
+            ["docker-compose", "--file", os.path.dirname(os.path.realpath(__file__)) + "/test-docker-compose.yml", "ps",
+             "--services", "--filter",
+             "status=running"]
+            , stdout=subprocess.PIPE, check=True)
+
+        if service.stdout == b'terminusdb-server\n':
+            try:
+                response = requests.get(test_url)
+                assert response.status_code == 200
+                break
+            except (requests.exceptions.ConnectionError, AssertionError):
+                pass
+
+        seconds_waited += 1
+        time.sleep(1)
+
+        if seconds_waited > MAX_CONTAINER_STARTUP_TIME:
+            clean_up_container()
+            raise RuntimeError("Container was to slow to startup")
+
     yield test_url
-    subprocess.run(["docker-compose", "down"])
-    subprocess.run(["docker-compose", "rm",  "--force", "--stop", "-v"])
+    clean_up_container()
+
+
+def clean_up_container():
+    subprocess.run(["docker-compose",
+                    "--file",
+                    os.path.dirname(os.path.realpath(__file__)) + "/test-docker-compose.yml", "down"], check=True)
+    subprocess.run(["docker-compose",
+                    "--file",
+                    os.path.dirname(os.path.realpath(__file__)) + "/test-docker-compose.yml", "rm", "--force", "--stop",
+                    "-v"], check=True)
 
 
 def test_main_service_run(docker_url):
