28ce63189032ec12245ab0aad20fd3c1d6513a68
rrooij <rrooij@users.noreply.github.com>
2021-01-11 12:43:57 +0000

    Deploying to gh-pages from  @ ce1e13703ffa409da41f2e58f86d5003999fc1f9 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '&lt;module &#39;terminusdb_client.__version__&#39; from &#39;/home/travis/build/terminusdb/terminusdb-client-python/terminusdb_client/__version__.py&#39;&gt;',
+    VERSION: '&lt;module &#39;terminusdb_client.__version__&#39; from &#39;/home/runner/work/terminusdb-client-python/terminusdb-client-python/terminusdb_client/__version__.py&#39;&gt;',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
