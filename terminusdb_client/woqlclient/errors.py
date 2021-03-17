b2f2c0d07eb83cbfed5396c19b119822ad90fc9a
Cheukting <cheukting.ho@gmail.com>
2021-03-17 11:23:11 +0000

    adding Error docstrings

--- terminusdb_client/woqlclient/errors.py
+++ terminusdb_client/woqlclient/errors.py
@@ -1,33 +1,45 @@
 class Error(Exception):
-    """Exception that is base class for all other error exceptions.
+    """Exception that is base class for all other error exceptions."""
+
+    def __init__(self):
+        super().__init__()
+
+
+class InterfaceError(Error):
+    """Exception for errors related to the interface.
 
     Attributes
     ----------
     message  : str
         Error message.
+
     """
-
-    def __init__(self, message=None, url=None, err_obj=None):
-        super().__init__()
-        self.message = message
-        self.url = url
-        self.error_obj = err_obj
-
-    def __str__(self):
-        return self.message
-
-
-class InterfaceError(Error):
-    """Exception for errors related to the interface."""
 
     def __init__(self, message):
         self.message = message
 
 
 class DatabaseError(Error):
-    """Exception for errors related to the database."""
+    """Exception for errors related to the database.
+
+    Attributes
+    ----------
+    message  : str
+        Error message.
+    error_obj : dict
+        Whole error object in json format
+    status_code : int
+        status_code from the request.Response
+    """
 
     def __init__(self, reponse=None):
+        """Exception for errors related to the database.
+
+        Parameters
+        ----------
+        reponse : request.Response
+            response from the api call
+        """
         super().__init__()
         if (
             reponse.headers["content-type"][len("application/json") :]
@@ -60,9 +72,34 @@ class AccessDeniedError(DatabaseError):
 
 
 class APIError(DatabaseError):
-    """Exceptions to do with return messages from HTTP"""
+    """Exceptions to do with return messages from HTTP
+
+    Attributes
+    ----------
+    message  : str
+        Error message.
+    error_obj : dict
+        Whole error object in json format
+    status_code : int
+        status_code from the request.Response
+    url : str
+        url causeing the Error
+    """
 
     def __init__(self, message=None, url=None, err_obj=None, status_code=None):
+        """Exceptions to do with return messages from HTTP
+
+        Parameters
+        ----------
+        message  : str
+            Error message.
+        error_obj : dict
+            Whole error object in json format
+        status_code : int
+            status_code from the request.Response
+        url : str
+            url causeing the Error
+        """
         super().__init__()
         self.message = message
         self.url = url
