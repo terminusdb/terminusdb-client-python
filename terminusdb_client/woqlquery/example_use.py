98c3371bbb3f1644885ddb15d5c334025e62f980
Cheukting <cheukting.ho@gmail.com>
2021-04-15 02:19:30 +0100

    adding domain and property automatically

--- terminusdb_client/woqlquery/example_use.py
+++ terminusdb_client/woqlquery/example_use.py
@@ -1,17 +1,32 @@
-from .woql_schema import Document, Property, WOQLObject
+from terminusdb_client.woqlquery.woql_schema import (
+    Document,
+    Enums,
+    Property,
+    WOQLObject,
+    WOQLSchema,
+)
+
+# from woql_schema import WOQLSchema, Document, Property, WOQLObject
 
 
 class Employee(Document):
-    properties = [AddressOf, ContactNum, ManagedBy, Title, TeamMemberOf]
+    # properties = [AddressOf, ContactNum, ManagedBy, Title, TeamMemberOf]
+    pass
 
 
 class Address(WOQLObject):
-    properties = [Postcode, StreetName, StreetNum, TownCity]
+    # properties = [Postcode, StreetName, StreetNum, TownCity]
+    pass
 
 
 class Team(Enums):
-    value_list = ["IT", "Marketing"]
+    value_set = {"IT", "Marketing"}
 
 
 class AddressOf(Property):
-    domain = []
+    domain = [Employee]
+
+
+my_schema = WOQLSchema([WOQLObject, Property])
+
+my_schema.commit(None)
