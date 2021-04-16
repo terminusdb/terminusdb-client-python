731eb1d586eae9f505927bee194a20574e9a1f52
Cheukting <cheukting.ho@gmail.com>
2021-04-16 12:39:36 +0100

    registry saved in schema

--- terminusdb_client/woqlquery/example_use.py
+++ terminusdb_client/woqlquery/example_use.py
@@ -8,40 +8,54 @@ from terminusdb_client.woqlquery.woql_schema import (
 
 # from woql_schema import WOQLSchema, Document, Property, WOQLObject
 
+my_schema = WOQLSchema()
+
 
 class Employee(Document):
     # properties = [AddressOf, ContactNum, ManagedBy, Title, TeamMemberOf]
-    pass
+    schema = my_schema
 
 
 class Address(WOQLObject):
     # properties = [Postcode, StreetName, StreetNum, TownCity]
-    pass
+    schema = my_schema
 
 
 class Team(Enums):
     """This is Team"""
 
     value_set = {"IT", "Marketing"}
+    schema = my_schema
 
 
 class AddressOf(Property):
-    domain = [Employee]
-    prop_range = [Address]
+    domain = {Employee}
+    prop_range = {Address}
+    schema = my_schema
 
 
-print(Team.__name__)
-
-my_schema = WOQLSchema()
-
+print(my_schema)
 my_schema.commit(None)
+my_other_schema = my_schema.copy()
+# my_other_schema = WOQLSchema()
 
 
 class Title(Property):
-    domain = [Employee]
-    prop_range = ["xsd:string"]
+    domain = {Employee}
+    prop_range = {"xsd:string"}
+    schema = my_other_schema
 
 
-my_other_schema = WOQLSchema()
+class Intern(Employee):
+    schema = my_other_schema
+
+
+# print("Intern")
+# print(Intern.properties)
 
 my_other_schema.commit(None)
+
+# for item in my_other_schema.all_prop():
+#     print(item)
+
+print(Team)
