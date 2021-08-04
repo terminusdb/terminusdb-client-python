70286f8bd13255c9bfefbd1ebb4ee1edf92dc3a5
Cheukting <cheukting.ho@gmail.com>
2021-08-04 17:36:30 +0100

    remove smart query

--- terminusdb_client/__init__.py
+++ terminusdb_client/__init__.py
@@ -1,9 +1,6 @@
 from .woqlclient import WOQLClient  # noqa
 from .woqldataframe import woqlDataframe as WOQLDataFrame  # noqa
-from .woqlquery import TerminusDB  # noqa
-from .woqlquery import WOQLClass  # noqa
 from .woqlquery import WOQLLib  # noqa
-from .woqlquery import WOQLObj  # noqa
 from .woqlquery import WOQLQuery  # noqa
 from .woqlschema import *  # noqa
 from .woqlview import WOQLView  # noqa
--- terminusdb_client/woqlquery/__init__.py
+++ terminusdb_client/woqlquery/__init__.py
@@ -1,3 +1,2 @@
-from .smart_query import TerminusDB, WOQLClass, WOQLObj  # noqa
 from .woql_library import WOQLLib  # noqa
 from .woql_query import WOQLQuery  # noqa
