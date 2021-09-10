08b2fe9169237f7647c7b7b8749f9a996be452e3
Cheukting <cheukting.ho@gmail.com>
2021-09-10 18:51:46 +0100

    Bump version: 10.0.0 → 10.0.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extras_require = {"dataframe": ["numpy >= 1.13.0", "pandas >= 0.23.0"]}
 
 setuptools.setup(
     name="terminusdb-client",
-    version="10.0.0",
+    version="10.0.1",
     author="TerminusDB group",
     author_email="terminusdatabase@gmail.com",
     description="Python client for Terminus DB",
