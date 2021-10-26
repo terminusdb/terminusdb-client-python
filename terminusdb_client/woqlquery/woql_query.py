e6f5e882892f05ee622cc92d51278f2a6a4f554e
Gavin Mendel-Gleason <gavin@terminusdb.com>
2021-10-26 10:04:13 +0200

    Add varible objects and document templates

--- terminusdb_client/woqlquery/woql_query.py
+++ terminusdb_client/woqlquery/woql_query.py
@@ -252,7 +252,6 @@ class WOQLQuery:
         return qobj
 
     def _raw_var(self, varb):
-        print(varb)
         if isinstance(varb, Var):
             return varb.name
         if varb[:2] == "v:":
