5f1b5552690e887425ef994676372d9fb13b8b43
Cheukting <cheukting.ho@gmail.com>
2022-06-02 21:02:46 +0000

    Deploying to gh-pages from @ terminusdb/terminusdb-client-python@a3d7daba8e77dae02488ae241a74b2e63801d10b 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '&lt;module &#39;terminusdb_client.__version__&#39; from &#39;/home/runner/work/terminusdb-client-python/terminusdb-client-python/terminusdb_client/__version__.py&#39;&gt;',
-    LANGUAGE: 'None',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
@@ -10,5 +10,5 @@ var DOCUMENTATION_OPTIONS = {
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: true,
+    ENABLE_SEARCH_SHORTCUTS: false,
 };
