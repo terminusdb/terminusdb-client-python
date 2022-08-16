40ce4196d405333c02c4065c95e3a8b9061fde08
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-08-16 14:48:37 +0200

    Remove uncaptured reference errors

--- terminusdb_client/tests/integration_tests/test_schema.py
+++ terminusdb_client/tests/integration_tests/test_schema.py
@@ -171,19 +171,6 @@ def test_insert_cheuk_again(docker_url, test_schema):
     cheuk.member_of = Team.information_technology
     cheuk._id = "Cheuk is back"
 
-    with pytest.raises(ValueError) as error:
-        client.update_document([uk])
-        assert (
-            str(error.value)
-            == f"{location._capture} is referenced but not captured. Seems you forgot to submit one or more object(s)."
-        )
-    with pytest.raises(ValueError) as error:
-        client.insert_document(uk)
-        assert (
-            str(error.value)
-            == "There are uncaptured references. Seems you forgot to submit one or more object(s)."
-        )
-
     client.update_document([location, uk, cheuk], commit_msg="Adding cheuk again")
     assert location._backend_id and location._id
     location.x = -0.7
