6d4567bed1d6ebd9fa5ce40632c23851bf37ee41
Robin de Rooij <rderooij685@gmail.com>
2022-08-22 15:18:01 +0200

    Remove WOQLView
    It was a cool feature but not maintained. It also had a very
    small percentage of test coverage which means that it was
    hard to detect regressions.

--- terminusdb_client/__init__.py
+++ terminusdb_client/__init__.py
@@ -2,7 +2,6 @@ from .client import Patch, Client  # noqa
 from .woqldataframe import woqlDataframe as WOQLDataFrame  # noqa
 from .woqlquery import WOQLQuery  # noqa
 from .woqlschema import *  # noqa
-from .woqlview import WOQLView  # noqa
 # Backwards compatibility
 WOQLClient = Client # noqa
 WOQLSchema = Schema # noqa
--- terminusdb_client/woqlview/__init__.py
+++
@@ -1,1 +0,0 @@
-from .woql_view import WOQLView  # noqa
