c1af6662abc96a226712440112d8818d741702cd
Robin de Rooij <rderooij685@gmail.com>
2022-08-23 14:34:39 +0200

    Increase version to 10.1.4

--- terminusdb_client/__version__.py
+++ terminusdb_client/__version__.py
@@ -1,9 +1,9 @@
 __title__ = "woqlClient"
 __description__ = "woqlClient library for accessing the Terminus DB API"
 __url__ = ""
-__version__ = "10.1.1"
+__version__ = "10.1.4"
 __build__ = 00
 __author__ = "TerminusDB group"
-__author_email__ = "terminusdatabase@gmail.com"
+__author_email__ = "team@terminusdb.com"
 __license__ = "GNU General Public License v3 (GPLv3)"
 __copyright__ = "Copyright 2019 datachemist"
