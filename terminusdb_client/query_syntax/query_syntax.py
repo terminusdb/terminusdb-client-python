22e057e092718b46e2b144a550621d31be0818c3
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-08-24 15:15:53 +0200

    Explicitly export only defined functions

--- terminusdb_client/query_syntax/query_syntax.py
+++ terminusdb_client/query_syntax/query_syntax.py
@@ -2,11 +2,14 @@ from ..woqlquery import WOQLQuery, Var, Doc
 import re as regexp
 import sys
 
-
 __module = sys.modules[__name__]
+
+__exported = []
+
 for attribute in dir(WOQLQuery()):
     if isinstance(attribute,str) and regexp.match('^[^_].*', attribute):
         print(attribute)
+        __exported.append(attribute)
         _woql_obj_fun = getattr(WOQLQuery(),attribute)
         def __create_a_function(function, *args, **kwargs):
             def __woql_fun(*args, **kwargs):
@@ -14,3 +17,5 @@ for attribute in dir(WOQLQuery()):
             return __woql_fun
 
         setattr(__module, attribute, __create_a_function(_woql_obj_fun))
+
+__all__ = __exported
