0cf311bf9bf2f9be2732540cfc9a58b617a0d8fd
Gavin Mendel-Gleason <gavin@terminusdb.com>
2022-08-24 21:57:26 +0200

    Fix tests for new query syntax

--- terminusdb_client/query_syntax/query_syntax.py
+++ terminusdb_client/query_syntax/query_syntax.py
@@ -2,26 +2,27 @@ from ..woqlquery import WOQLQuery, Var, Doc # noqa
 import re
 import sys
 
-__BARRED = ['re','vars']
-__ALLOWED = ['__and__','__or__','__add__']
+__BARRED = ['re', 'vars']
+__ALLOWED = ['__and__', '__or__', '__add__']
 __module = sys.modules[__name__]
-
 __exported = ['Var', 'Doc']
+
+
+def __create_a_function(attribute):
+    def __woql_fun(*args, **kwargs):
+        obj = WOQLQuery()
+        func = getattr(obj, attribute)
+        return func(*args, **kwargs)
+    return __woql_fun
+
 
 for attribute in dir(WOQLQuery()):
     if (isinstance(attribute, str) and (re.match('^[^_].*', attribute)
                                         or attribute in __ALLOWED)
-            and not attribute in __BARRED):
+            and attribute not in __BARRED):
 
         __exported.append(attribute)
 
-        def __create_a_function():
-            obj = WOQLQuery()
-            func = getattr(obj,attribute)
-            def __woql_fun(*args, **kwargs):
-                return func(*args, **kwargs)
-            return __woql_fun
-
-        setattr(__module, attribute, __create_a_function())
+        setattr(__module, attribute, __create_a_function(attribute))
 
 __all__ = __exported
