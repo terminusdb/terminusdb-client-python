2e968a615c1cc38e3274b1feff2a5890dc231545
Robin de Rooij <rderooij685@gmail.com>
2023-05-30 14:19:22 +0200

    Add version 10.2.3

--- terminusdb_client/__version__.py
+++ terminusdb_client/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "woqlClient"
 __description__ = "woqlClient library for accessing the Terminus DB API"
 __url__ = ""
-__version__ = "10.2.2"
+__version__ = "10.2.3"
 __build__ = 00
 __author__ = "TerminusDB group"
 __author_email__ = "team@terminusdb.com"
