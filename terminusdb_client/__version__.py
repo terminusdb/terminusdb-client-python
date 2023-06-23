2a3c8cc92ed7d2650b0c1f40d4b13b7e8964897c
Robin de Rooij <rrooij@users.noreply.github.com>
2023-06-23 16:56:42 +0200

    New docs generation (#405)
    * Fix docs and license
    
    * Try to insert docs
    
    * Use poetry
    
    * Add commit message and author
    
    * Remove the commit message and author
    
    * Fix integration tests because of new requests lib (#395)
    
    * Fix conversion of sys:JSON type schema to Python (#400)
    
    If a class contained a "sys:JSON" property, it wouldn't
    convert to a normal Python class as it should.
    
    Fixes issue #397
    
    * Add version 10.2.3
    
    * Change title of application and description
    
    * Get examples
    
    * ci: don't do poetry interactively
    
    * Fix parameters for patch function
    
    * Only generate docs on version push or manual run

--- terminusdb_client/__version__.py
+++ terminusdb_client/__version__.py
@@ -1,9 +1,9 @@
-__title__ = "woqlClient"
-__description__ = "woqlClient library for accessing the Terminus DB API"
+__title__ = "terminusdb-client"
+__description__ = "TerminusDB client library for accessing the Terminus DB API"
 __url__ = ""
 __version__ = "10.2.3"
 __build__ = 00
 __author__ = "TerminusDB group"
 __author_email__ = "team@terminusdb.com"
-__license__ = "GNU General Public License v3 (GPLv3)"
+__license__ = "Apache 2.0"
 __copyright__ = "Copyright 2019 datachemist"
